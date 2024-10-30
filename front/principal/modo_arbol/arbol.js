f2487079eaa591c4767fd5dd983a54afbb3b78aa
JoacoSch <joacoschvartzman@gmail.com>
2024-10-29 23:41:01 -0300

    arbol

--- front/principal/modo_arbol/arbol.js
+++ front/principal/modo_arbol/arbol.js
@@ -1,18 +1,37 @@
-let counter = 0;
+let fruitExist = false
+const fruitTypes = {
+    banana: { id: 0, won: 0 },
+    apple: { id: 1, won: 0 },
+    grapes: { id: 2, won: 0 },
+    pineapple: { id: 3, won: 0 },
+    dragonFruit: { id: 4, won: 0 }
+};
+
+let cords = [
+    { top: "50%", left: "10%" },
+    { top: "50%", left: "20%" },
+    { top: "50%", left: "30%" },
+    { top: "50%", left: "40%" },
+    { top: "50%", left: "50%" },
+    { top: "50%", left: "60%" },
+    { top: "50%", left: "70%" },
+    { top: "50%", left: "80%" },
+    { top: "50%", left: "90%" },
+    { top: "50%", left: "100%" },
+];
 const counterDisplay = document.getElementById('counter');
 const fruits = Array.from(document.querySelectorAll('.fruit'));
 
-// Actualiza el contador en pantalla
-function updateCounter() {
-    counterDisplay.textContent = counter;
-}
-
-// Muestra una fruta al azar
 function showRandomFruit() {
+    if(!fruitExist){
     console.log("fruta")
-    const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+    window.randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+    let cord = cords[Math.floor(Math.random() * cords.length)];
+    randomFruit.style.top = cord.top;
+    randomFruit.style.left = cord.left
     randomFruit.classList.remove('hidden');
-    console.log("fruta2")
+    fruitExist = true
+}
 }
 
 // Maneja el clic en el gnomo
@@ -27,9 +46,8 @@ document.getElementById('gnomo').addEventListener('click', () => {
 fruits.forEach(fruit => {
     fruit.addEventListener('click', () => {
         if (!fruit.classList.contains('hidden')) {
-            counter += 1;
-            updateCounter();
-            fruit.classList.add('hidden');  // Oculta la fruta de nuevo
+            fruitExist = false
+            fruit.classList.add('hidden');  
         }
     });
 });
