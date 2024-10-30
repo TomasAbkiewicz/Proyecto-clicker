fbe333994f668851b9f0ff2b920f429792904911
tomas <tomasgarciab30@gmail.com>
2024-10-29 23:41:40 -0300

    nada

--- front/principal/modo_arbol/arbol.js
+++ front/principal/modo_arbol/arbol.js
@@ -1,6 +1,21 @@
 let counter = 0;
+let fruitIsShown= false;
 const counterDisplay = document.getElementById('counter');
 const fruits = Array.from(document.querySelectorAll('.fruit'));
+const positions = [
+    { top: '5%', left: '10%' },
+    { top: '10%', left: '20%' },
+    { top: '15%', left: '30%' },
+    { top: '20%', left: '40%' },
+    { top: '25%', left: '50%' },
+    { top: '10%', left: '60%' },
+    { top: '30%', left: '70%' },
+    { top: '35%', left: '80%' },
+    { top: '40%', left: '90%' },
+    { top: '25%', left: '80%' },
+];
+
+
 
 // Actualiza el contador en pantalla
 function updateCounter() {
@@ -9,16 +24,23 @@ function updateCounter() {
 
 // Muestra una fruta al azar
 function showRandomFruit() {
-    console.log("fruta")
-    const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
-    randomFruit.classList.remove('hidden');
-    console.log("fruta2")
+    if (!fruitIsShown){
+        fruitIsShown= true
+        console.log("fruta")
+        const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+        randomFruit.classList.remove('hidden');
+        console.log("fruta2")
+        const randomPosition = positions[Math.floor(Math.random() * positions.length)];
+        randomFruit.style.top = randomPosition.top;
+        randomFruit.style.left = randomPosition.left;
+    }
+   
 }
 
 // Maneja el clic en el gnomo
 document.getElementById('gnomo').addEventListener('click', () => {
     console.log("gnomo click")
-    if (Math.random() < 0.5) {  // 50% de probabilidad de mostrar una fruta
+    if (Math.random() < 0.05) {  // 50% de probabilidad de mostrar una fruta
         showRandomFruit();
     }
 });
@@ -28,6 +50,7 @@ fruits.forEach(fruit => {
     fruit.addEventListener('click', () => {
         if (!fruit.classList.contains('hidden')) {
             counter += 1;
+            fruitIsShown =false;
             updateCounter();
             fruit.classList.add('hidden');  // Oculta la fruta de nuevo
         }
